fa908ad12fdb923082ca0e5d8aa870e04c9df67a
MauroMieres <mauronicolasmieres@gmail.com>
2022-06-10 20:49:15 -0300

    TP REALIZADO

---
+++ main.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+#include "Controller.h"
+#include "Passenger.h"
+#include "menus.h"
+
+/****************************************************
+    Menu:
+     1. Cargar los datos de los pasajeros desde el archivo data.csv (modo texto).
+     2. Cargar los datos de los pasajeros desde el archivo data.csv (modo binario).
+     3. Alta de pasajero
+     4. Modificar datos de pasajero
+     5. Baja de pasajero
+     6. Listar pasajeros
+     7. Ordenar pasajeros
+     8. Guardar los datos de los pasajeros en el archivo data.csv (modo texto).
+     9. Guardar los datos de los pasajeros en el archivo data.csv (modo binario).
+    10. Salir
+*****************************************************/
+
+int main()
+{
+	setbuf(stdout,NULL);
+    int option = 0;
+    int contadorPasajeros=0;
+    LinkedList* listaPasajeros = ll_newLinkedList();
+    do{
+        switch(menuOpcion())
+        {
+
+        case 1:
+        			controller_loadFromText("data.csv",listaPasajeros);
+        			contadorPasajeros = ll_len(listaPasajeros);
+        			break;
+        		case 2:
+        			controller_loadFromBinary("data.bin",listaPasajeros);
+        			contadorPasajeros = ll_len(listaPasajeros);
+        			break;
+        		case 3:
+        			controller_addPassenger(listaPasajeros);
+        			contadorPasajeros = ll_len(listaPasajeros);
+        			break;
+        		case 4:
+        			if(contadorPasajeros>0)
+        			{
+        				controller_editPassenger(listaPasajeros);
+        			}
+        			else
+        			{
+        				printf("No hay pasajeros cargados en el sistema\n");
+        			}
+        			break;
+        		case 5:
+        			if(contadorPasajeros>0)
+        			{
+        			controller_removePassenger(listaPasajeros);
+        			}
+        			else
+        			{
+        				printf("No hay pasajeros cargados en el sistema\n");
+        			}
+        			break;
+        		case 6:
+        			if(contadorPasajeros>0)
+        			{
+        				controller_ListPassenger(listaPasajeros);
+        			}
+        			else
+					{
+						printf("No hay pasajeros cargados en el sistema\n");
+					}
+        			break;
+        		case 7:
+        			if(contadorPasajeros>0)
+        			{
+        				controller_sortPassenger(listaPasajeros);
+        			}
+        			else
+					{
+						printf("No hay pasajeros cargados en el sistema\n");
+					}
+        			break;
+        		case 8:
+        			controller_saveAsText("data.csv", listaPasajeros);
+        			break;
+        		case 9:
+        			controller_saveAsBinary("data.bin", listaPasajeros);
+        			break;
+        		case 10:
+        			printf("Salir\n");
+        			break;
+        		default:
+        			printf("Opcion invalida!\n");
+        			break;
+        }
+    }while(option != 10);
+    ll_deleteLinkedList(listaPasajeros);
+    return 0;
+}
+
